b34c8b4a76ca2f53692e7697c229e03d3fc32a9e
Dmytro Kovalenko <dmytrokovalenko.new@gmail.com>
2024-03-12 15:57:37 -0700

    feat: Added component Projects

--- app/theme/template-home.php
+++ app/theme/template-home.php
@@ -8,11 +8,13 @@
 get_header();
 $args = array(
 	'post_type'     => 'projects',
+    'posts_per_page' => 6,
 );
 $projects_home_posts = new WP_Query( $args );
 ?>
     <main class="projectSinglePost__page">
         <section class="content_wrapper">
+            <div class="headline"><?= __('Our cases')?></div>
             <div class="projects">
 	            <?= get_post_meta( get_the_ID(), 'homePage_description', true ) ?>
 
